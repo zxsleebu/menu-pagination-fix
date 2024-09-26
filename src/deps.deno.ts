aaa9a3767b57bc6091ffddb1577aa3b505d13772
sleebu <46263989+zxsleebu@users.noreply.github.com>
2024-09-26 22:53:14 +0200

    Update deps.deno.ts

--- src/deps.deno.ts
+++ src/deps.deno.ts
@@ -3,6 +3,6 @@ export type {
     Context,
     SessionFlavor,
 } from "https://deno.land/x/grammy@v1.19.2/mod.ts";
-export { MenuRange } from "https://deno.land/x/grammy_menu@v1.2.1/mod.ts";
-export type { MenuFlavor } from "https://deno.land/x/grammy_menu@v1.2.1/mod.ts ";
+export { MenuRange } from "https://deno.land/x/grammy_menu@v1.2.2/mod.ts";
+export type { MenuFlavor } from "https://deno.land/x/grammy_menu@v1.2.2/mod.ts";
 export { readJson, writeJson } from "https://deno.land/x/jsonfile@1.0.0/mod.ts";
